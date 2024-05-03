9ed467ef7a3a652c50993a79bf58b3ea08a20db6
hyoon1129 <hyoon1129@naver.com>
2024-05-03 20:17:46 +0900

    feat:caregiver, schedule 도메인 초기 설정

--- src/main/java/hongikchildren/carefriends/domain/Caregiver.java
+++ src/main/java/hongikchildren/carefriends/domain/Caregiver.java
@@ -1,4 +1,42 @@
 package hongikchildren.carefriends.domain;
 
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor
 public class Caregiver {
+
+    @Id @GeneratedValue
+    @Column(name = "caregiverId")
+    private Long id;
+
+    private String name;
+
+    private String phoneNumber;
+
+    private String gender;
+
+    private LocalDate birthDate;
+
+    @OneToMany(mappedBy = "caregiver")
+    private List<Friends> friends =new ArrayList<>();
+
+
+    @Builder
+    protected Caregiver(Long id, String name, String phoneNumber, String gender, LocalDate birthDate, List<Friends> friends) {
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.gender = gender;
+        this.birthDate = birthDate;
+        this.friends = friends;
+    }
 }
--- src/main/java/hongikchildren/carefriends/repository/CaregiverRepository.java
+++ src/main/java/hongikchildren/carefriends/repository/CaregiverRepository.java
@@ -1,4 +1,12 @@
 package hongikchildren.carefriends.repository;
 
-public class CaregiverRepository {
+import hongikchildren.carefriends.domain.Caregiver;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CaregiverRepository extends JpaRepository<Caregiver, Long> {
+    Optional<Caregiver> findByCaregiverId(Long caregiverId);
+    List<Caregiver> findByCaregiverName(String caregiverName);
 }
