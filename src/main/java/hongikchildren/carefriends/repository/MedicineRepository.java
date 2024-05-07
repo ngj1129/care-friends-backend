6073b7c87b1e986c5e9911a1e380154a9af2d0ba
나경주 <nagyeongju@nagyeongjuui-noteubug.local>
2024-05-07 21:14:11 +0900

    feat: Medicine Service 코드
    - 추가, 조회, 삭제코드 추가

--- src/main/java/hongikchildren/carefriends/repository/MedicineRepository.java
+++ src/main/java/hongikchildren/carefriends/repository/MedicineRepository.java
@@ -1,6 +1,7 @@
 package hongikchildren.carefriends.repository;
 
 import hongikchildren.carefriends.domain.Caregiver;
+import hongikchildren.carefriends.domain.Friends;
 import hongikchildren.carefriends.domain.Medicine;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -10,4 +11,7 @@ import java.util.Optional;
 public interface MedicineRepository extends JpaRepository<Medicine, Long> {
     Optional<Medicine> findById(Long id);
     List<Medicine> findByName(String name);
+
+    //spring data jpa가 프록시 객체 생성하고 조인 쿼리 생성해줌.
+    List<Medicine> findByFriends(Friends friends);
 }
