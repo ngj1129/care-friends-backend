5f894158d37a888a8ca89ebc9b3efbf3fc192b47
ngj1129 <grw13141@naver.com>
2025-01-23 22:18:40 +0900

    feat: 영상통화를 위한 시그널링 서버 구축

--- src/main/java/hongikchildren/carefriends/jwt/SecurityConfig.java
+++ src/main/java/hongikchildren/carefriends/jwt/SecurityConfig.java
@@ -66,6 +66,7 @@ public class SecurityConfig {
 
         http
                 .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/ws/**", "/signal/**").permitAll()
                         .requestMatchers("/login", "/", "/join", "/signup", "/kakao/callback").permitAll()
                         .anyRequest().authenticated());
 
