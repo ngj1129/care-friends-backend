e9a9dbe08e08132c5b99c40cde9320d79fb5993d
ngj1129 <grw13141@naver.com>
2025-02-18 12:33:19 +0900

    feat: aop 추가
    api 요청마다 로그 출력

--- src/main/java/hongikchildren/carefriends/FilterConfig.java
+++ src/main/java/hongikchildren/carefriends/FilterConfig.java
@@ -12,7 +12,7 @@ public class FilterConfig {
         FilterRegistrationBean<RequestLoggingFilter> registrationBean = new FilterRegistrationBean<>();
 
         registrationBean.setFilter(new RequestLoggingFilter());
-        registrationBean.addUrlPatterns("/friendRequest/*"); // 필터를 특정 경로에만 적용할 때
+//        registrationBean.addUrlPatterns("/friendRequest/*"); // 필터를 특정 경로에만 적용할 때
         registrationBean.setOrder(1); // 필터의 우선순위 지정
 
         return registrationBean;
